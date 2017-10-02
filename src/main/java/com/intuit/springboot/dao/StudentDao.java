33f82cfcd6ca1b960074b53ef8959f75fbeab3ea
Akshatha Udupa <Akshatha_Udupa@intuit.com>
2017-10-02 18:04:52 +0100

    Initial commit

---
+++ src/main/java/com/intuit/springboot/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.intuit.springboot.dao;
+
+import com.intuit.springboot.entity.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudentToDb(Student student);
+}
